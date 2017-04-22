345d5b052268bb972ada9257ea8787c90e200444
linheimx@gmail.com <linheimx333666>
2017-04-22 18:19:38 +0800

    add waveview

--- app/src/main/java/com/linheimx/zimudog/MainActivity.java
+++ app/src/main/java/com/linheimx/zimudog/MainActivity.java
@@ -1,14 +1,32 @@
 package com.linheimx.zimudog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.support.v7.app.AppCompatActivity;
+
+import com.linheimx.zimudog.vp.custview.WaveView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.wave)
+    WaveView wave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        wave.setDuration(7000);
+        wave.setStyle(Paint.Style.FILL);
+        wave.setColor(Color.BLUE);
+        wave.setInterpolator(new LinearOutSlowInInterpolator());
+        wave.start();
     }
 }
