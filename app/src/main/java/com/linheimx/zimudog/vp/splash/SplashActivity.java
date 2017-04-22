33e9d2d20308d1f6eeb34a3ac01c71a58183d015
linheimx@gmail.com <linheimx333666>
2017-04-22 18:11:15 +0800

    add splash ac

---
+++ app/src/main/java/com/linheimx/zimudog/vp/splash/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.linheimx.zimudog.vp.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.linheimx.zimudog.MainActivity;
+import com.linheimx.zimudog.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.animation_view)
+    LottieAnimationView animationView;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        tvTitle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1800);
+    }
+}
